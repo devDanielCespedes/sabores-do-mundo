ce8ed677833a365ab8206329b76dfa38ea99903d
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-02-11 13:36:58 -0300

    Updates

---
+++ precache-manifest.6575f7980f6bbb15d581ddd5c1118029.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0704ce7d64e84699eab80951ec946d53",
+    "url": "/sabores-do-mundo/index.html"
+  },
+  {
+    "revision": "7244637ee5b773431bfb",
+    "url": "/sabores-do-mundo/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "98a5decd471e163a3f19",
+    "url": "/sabores-do-mundo/static/css/main.c474f288.chunk.css"
+  },
+  {
+    "revision": "7244637ee5b773431bfb",
+    "url": "/sabores-do-mundo/static/js/2.099089f1.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/sabores-do-mundo/static/js/2.099089f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a5decd471e163a3f19",
+    "url": "/sabores-do-mundo/static/js/main.1cdf10ce.chunk.js"
+  },
+  {
+    "revision": "f73a2f44d0702532765a",
+    "url": "/sabores-do-mundo/static/js/runtime-main.1b8e7d83.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/sabores-do-mundo/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/sabores-do-mundo/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/sabores-do-mundo/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "88a6db57627d6b4f6b046f5f26a6f465",
+    "url": "/sabores-do-mundo/static/media/facebookIcon.88a6db57.png"
+  },
+  {
+    "revision": "dc610311496ff8edfb022a6ac6b74806",
+    "url": "/sabores-do-mundo/static/media/instagramIcon.dc610311.jpg"
+  },
+  {
+    "revision": "260d496fd401dddb28fc8f69a2b8fd62",
+    "url": "/sabores-do-mundo/static/media/logoDoAppEscrita.260d496f.jpg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/sabores-do-mundo/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/sabores-do-mundo/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/sabores-do-mundo/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/sabores-do-mundo/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "461089b8b31602c9c75ea2c022e55783",
+    "url": "/sabores-do-mundo/static/media/whatsAppIcon.461089b8.jpg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/sabores-do-mundo/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
